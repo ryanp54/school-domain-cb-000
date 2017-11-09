2681093c77a2e41cc18299717dc5bc81f10153a9
Ryan Pfeifer <pfeifer.ryan@gmail.com>
2017-11-09 01:22:54 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -8,16 +8,16 @@ class School
   end
 
   def add_student(student, grade)
-       @roster[grade] ? @roster[grade] << student : @roster[grade] = [student]
+       roster[grade] ? roster[grade] << student : roster[grade] = [student]
   end
 
   def grade(grade)
-    @roster[grade]
+    roster[grade]
   end
 
   def sort
     sorted_roster = {}
-    @roster.each { |grade, students| sorted_roster[grade] = students.sort }
+    roster.each { |grade, students| sorted_roster[grade] = students.sort }
     sorted_roster
   end
 
